36c9ae928f97a49ae7e1d7954deb53fc25d39711
Pedro Neto <pedroneto@pecege.com>
2025-02-25 19:11:52 -0300

    Pass View

--- src/screens/login/index.jsx
+++ src/screens/login/index.jsx
@@ -7,10 +7,13 @@ import {
 } from "react-firebase-hooks/auth";
 import { auth } from "../../services/firebaseConfig";
 import { sendPasswordResetEmail } from "firebase/auth";
+import { EyeSlashIcon } from "@heroicons/react/24/outline";
+import { EyeIcon } from "@heroicons/react/24/outline";
 
 const Login = () => {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [viewPass, setViewPass] = useState(true);
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth);
   const navigate = useNavigate();
@@ -42,28 +45,46 @@ const Login = () => {
   // }, [theme]);
 
   return (
-    <div className="flex flex-col w-screen h-screen justify-start items-center pt-48">
+    <div className="flex flex-col w-screen h-screen justify-start items-center pt-48 pr-5">
       <img className="w-52 h-44" src={logo}></img>
       <div className="flex flex-col items-center mt-12">
         <h1 className="text-3xl font-bold">Entre com sua conta</h1>
       </div>
-      <div className="flex flex-col w-96 mt-8">
+      <div className="flex flex-col w-auto mt-8 items-center justify-center">
         <input
-          className="border border-gray-300 p-2 mb-1 rounded-md focus:outline-black"
+          className="border border-gray-300 p-2 mb-1 rounded-md focus:outline-black w-96"
           type="text"
           placeholder="E-mail"
           onChange={(e) => {
             setEmail(e.target.value);
           }}
         ></input>
-        <input
-          className="border border-gray-300 p-2 rounded-md focus:outline-black"
-          type="password"
-          placeholder="Senha"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        ></input>
+        <div className="flex flex-row">
+          <input
+            className="border border-gray-300 p-2 rounded-md focus:outline-black w-96 ml-12"
+            type={
+              viewPass == true ? "text" : viewPass == false ? "password" : ""
+            }
+            placeholder="Senha"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              setViewPass(!viewPass);
+            }}
+            className="pl-2 pr-4"
+          >
+            {viewPass == true ? (
+              <EyeSlashIcon className="size-6 text-gray-700" />
+            ) : viewPass == false ? (
+              <EyeIcon className="size-6 text-gray-700" />
+            ) : (
+              ""
+            )}
+          </button>
+        </div>
       </div>
       <div className="flex flex-col w-96 mt-8">
         <button
